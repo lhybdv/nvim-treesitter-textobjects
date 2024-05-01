53da8fe22dcae9b4fe57f853396fa897337dc845
lhybdv <itelite39660787@gmail.com>
2024-05-01 21:46:38 +0800

    feat(vue): support css

--- queries/vue/textobjects.scm
+++ queries/vue/textobjects.scm
@@ -4,4 +4,17 @@
   (attribute)
   (directive_attribute)
 ] @attribute.outer
+
 (attribute_value) @attribute.inner
+
+; support css
+(attribute_name) @assignment.lhs
+(attribute_value) @assignment.rhs
+
+((attribute_name) 
+.
+":"
+.
+(attribute_value)
+) @assignment.outer
+
